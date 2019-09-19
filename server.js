b1e9491fba9c377a9b8bea537023f14eae37164f
Adnan <adnan.chowdhury@outlook.com>
2019-09-19 17:34:39 -0500

    delete endpoint created on spec

--- server.js
+++ server.js
@@ -13,4 +13,22 @@ server.get("/users", (req, res) => {
   res.status(200).json(userArray);
 });
 
+server.post("/users", (req, res) => {
+  if (req.body.name) {
+    userArray.push(req.body);
+    res.status(201).json({ message: "User was added", user: req.body });
+  } else {
+    res.status(401).json({ message: "No data was detected" });
+  }
+});
+
+server.delete("/users", (req, res) => {
+  if (req.body.name) {
+    userArray = userArray.filter(del => del.name !== req.body.name);
+    res.status(201).json({ message: "User was deleted", user: req.body });
+  } else {
+    res.status(401).json({ message: "No data was detected" });
+  }
+});
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -20,4 +20,38 @@ describe("Running Server", () => {
       expect(res.body.length).toEqual(3);
     });
   });
+  describe("POST Users/", () => {
+    it("returns user added successfully", async () => {
+      const newUser = { name: "Jade" };
+      const res = await request(server)
+        .post("/users")
+        .send(newUser);
+      expect(res.status).toBe(201);
+      //Retreive the user added
+      const expected = [{ name: "Jade" }];
+      const nextres = await request(server).get("/users");
+      expect(nextres.body).toEqual(expect.arrayContaining(expected));
+    });
+    it("POST request checks if there is no data", async () => {
+      const res = await request(server).post("/users");
+      expect(res.status).toBe(401);
+    });
+  });
+  describe("Delete a User", () => {
+    it("returns user is successfully deleted and Checks new length", async () => {
+      const deleteUser = { name: "Adnan" };
+      const res = await request(server)
+        .delete("/users")
+        .send(deleteUser);
+
+      expect(res.status).toBe(201);
+      const expected = [{ name: "Adnan" }];
+      const newres = await request(server).get("/users");
+      expect(newres.body).toEqual(expect.not.arrayContaining(expected));
+    });
+    it("Delete request checks if there is no user", async () => {
+      const res = await request(server).delete("/users");
+      expect(res.status).toBe(401);
+    });
+  });
 });
